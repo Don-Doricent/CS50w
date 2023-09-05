c1150cae3b14164d85de6226bb23e6b9b96e613d
Donald Doricent <ddoricent97@gmail.com>
2023-09-05 14:08:50 -0400

    learning sets.

---
+++ Python/sets.py
@@ -0,0 +1,15 @@
+# Create an empty set
+s = set()
+
+# Add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+
+s.remove(2)
+print(s)
+
+# len will give the length of a sequence
+
+print(f"The set has {len(s)} elements.")
