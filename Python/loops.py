dc16f123ffb46f5ca667c9346c13d0036259a800
Donald Doricent <ddoricent97@gmail.com>
2023-09-05 14:13:06 -0400

    loops.

--- Python/loops.py
+++ Python/loops.py
@@ -1,2 +1,10 @@
+#one example of a loop
 for i in [0,1,2,3,4,5]:
     print(i)
+#Another
+    for i in range(6):
+        print(i)
+#Another
+        names = ["Hary", "Ron", "Hermione"]
+        for name in names:
+            print(name)
